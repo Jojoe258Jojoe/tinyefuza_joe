1c392b6125c09f1bb5d3a0779598f5a69c649908
Tinyefuza Joe <tinyejoe7@gmail.com>
2025-05-24 13:29:13 +0300

    Update exercise.c

--- exercise.c
+++ exercise.c
@@ -7,25 +7,27 @@ int main() {
     struct Book {
         char title[50];
         char author[50];
+        float price;
     };
 
     struct Book book;
     FILE *file;
 
-    printf("Enter book title: ");    // Input book details
+    printf("Enter book title: ");    // Input book title 
     fgets(book.title, sizeof(book.title), stdin)
 
-    printf("Enter author name: ");
+    printf("Enter author name: ");     //input book author name 
     fgets(book.author, sizeof(book.author), stdin);
 
     // Saving to file
     file = fopen("book_details.txt", "w");
-    
+
+    //checking whether the file was created or exists 
     if (file == NULL) {
         printf("Error opening file!\n");
         return 1;
     }
-    fprintf(file, "%s\n%s\n", book.title, book.author);
+    fprintf(file, "%s\n%s\n%.2f\n", book.title, book.author, book.price);
     fclose(file);
     printf("\nBook details saved to book_details.txt\n");
 
@@ -43,6 +45,10 @@ int main() {
     printf("Author: ");  //display book author
     fgets(book.author, sizeof(book.author), file);
     printf("%s\n", book.author);
+
+    float price;
+    fscanf(file, "%f", &price);
+    printf("Price: %.2f\n", price);
 
     fclose(file);
 
